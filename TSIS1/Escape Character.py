c9fcd736b95f1d621d5f2122051f8674e91021b9
SunkarAhinov <123620671+SunkarAhinov@users.noreply.github.com>
2023-01-31 19:57:36 +0600

    Add files via upload

---
+++ TSIS1/Escape Character.py
@@ -0,0 +1,38 @@
+                                   #Escape Character
+txt = "We are the so-called \"Vikings\" from the north."
+
+
+\'	    Single Quote
+txt = 'It\'s alright.'
+print(txt) 
+	
+\\	    Backslash	
+txt = "This will insert one \\ (backslash)."
+print(txt) 
+
+\n	    New Line	
+txt = "Hello\nWorld!"
+print(txt) 
+
+\r	    Carriage Return	
+txt = "Hello\rWorld!"
+print(txt) 
+
+\t	    Tab	
+txt = "Hello\tWorld!"
+print(txt) 
+
+\b	    Backspace	
+txt = "Hello \bWorld!"
+print(txt) 
+
+\f	    Form Feed
+	
+\ooo	Octal value	
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt) 
+
+\xhh	Hex value
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
---
+++ TSIS1/Variable Names.py
@@ -0,0 +1,15 @@
+#Legal variable names:                 #Variable Names
+a="John"
+myvar = a
+my_var = a
+_my_var = a
+myVar = a
+MYVAR = a
+myvar2 = a
+
+
+#Illegal variable names:
+
+2myvar = a
+my-var = a
+my var = a
